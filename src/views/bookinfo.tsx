f1e84091e189925c4554b2a76953457f8b658737
nzadkovsky <nathan@pathspottech.com>
2024-10-04 16:41:11 -0400

    added more pg count, publiser info, todos for status and update progress, and genres for book info page

--- src/views/bookinfo.tsx
+++ src/views/bookinfo.tsx
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   NativeSyntheticEvent,
   TextLayoutEventData,
+  Pressable,
 } from 'react-native';
 import { SearchResult } from '../data/books';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -55,8 +56,9 @@ export const BookInfo = ({ navigation, route }: Props) => {
   const handleTextLayout = (
     event: NativeSyntheticEvent<TextLayoutEventData>,
   ) => {
+    // might need to calc expected line height with this
     const { lines } = event.nativeEvent;
-    if (lines.length > DESCRIPTION_NUMBER_OF_LINES) {
+    if (lines.length == DESCRIPTION_NUMBER_OF_LINES) {
       setDescriptionTruncatd(true);
     }
   };
@@ -79,29 +81,59 @@ export const BookInfo = ({ navigation, route }: Props) => {
         <View style={styles.right}>
           <Text style={styles.title}>{book.volumeInfo.title}</Text>
           <Text style={styles.subtitle}>{book.volumeInfo.authors}</Text>
-          <Text style={styles.subtitle}>{book.volumeInfo.categories}</Text>
+
+          {/* page info */}
+          <Text style={styles.subtitle}>
+            {book.volumeInfo.pageCount + ' pages'}
+          </Text>
+
+          {/* publishing info */}
+          <Text style={styles.subtitle}>{book.volumeInfo.publisher}</Text>
+          <Text style={styles.subtitle}>{book.volumeInfo.publishedDate}</Text>
+
+          {/* TODO : status section */}
+          <Pressable onPress={() => {}}>
+            <Text>TODO Status</Text>
+          </Pressable>
+
+          {/* update progress */}
+          <Pressable onPress={() => {}}>
+            <Text>TODO update progress</Text>
+          </Pressable>
         </View>
       </View>
 
       {/* description info */}
-      <View style={styles.descriptionContainer}>
-        <Text
-          ref={descriptionRef}
-          style={styles.subtitle}
-          numberOfLines={
-            descriptionExpanded ? SHOW_ALL_LINES : DESCRIPTION_NUMBER_OF_LINES
-          }
-          onTextLayout={handleTextLayout}
-          ellipsizeMode="tail">
-          {book.volumeInfo.description}
-        </Text>
+      <View style={styles.additionalInfoContainer}>
+        {/* generes */}
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Genres: </Text>
+          {/* maybe move this to its own block */}
+          <Text style={styles.subtitle}>{book.volumeInfo.categories}</Text>
+        </View>
 
-        {/* if description is truncated, add epand/hide option */}
-        {descriptionTruncated && (
-          <Text onPress={handleExpandPress} style={{ fontWeight: '600' }}>
-            {descriptionExpanded ? 'Read less' : 'Read more...'}
+        {/* description */}
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Description </Text>
+
+          <Text
+            ref={descriptionRef}
+            style={styles.subtitle}
+            numberOfLines={
+              descriptionExpanded ? SHOW_ALL_LINES : DESCRIPTION_NUMBER_OF_LINES
+            }
+            onTextLayout={handleTextLayout}
+            ellipsizeMode="tail">
+            {book.volumeInfo.description}
           </Text>
-        )}
+
+          {/* if description is truncated, add epand/hide option */}
+          {descriptionTruncated && (
+            <Text onPress={handleExpandPress} style={{ fontWeight: '600' }}>
+              {descriptionExpanded ? 'Read less' : 'Read more...'}
+            </Text>
+          )}
+        </View>
       </View>
     </View>
   );
@@ -141,6 +173,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '600',
     color: 'grey',
+    marginTop: '1%',
   },
-  descriptionContainer: { justifyContent: 'center', margin: '2%' },
+  additionalInfoContainer: { justifyContent: 'center', margin: '2%' },
+  sectionContainer: {
+    justifyContent: 'center',
+    margin: '2%',
+  },
+  sectionTitle: { fontWeight: '600' },
 });
